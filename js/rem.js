6d860e2084176a515fb97aaab0902acdb1bc0e23
XiongAmao <onycat@live.cn>
2017-06-22 21:57:58 +0800

    rem动态方案迁移出来

---
+++ js/rem.js
@@ -0,0 +1,29 @@
+// REM init
+!function(){
+  function remInit() {
+    var dpr = window.devicePixelRatio || 1
+    var docEl = document.documentElement
+    var clientWidth = docEl.clientWidth
+
+    var dprs = [2, 3]
+    var metaEl = document.querySelector('meta[name="viewport"]')
+    dpr = !dprs.indexOf(dpr) ? dpr : 1
+    // dpr = dprs.some(function(elem){ return elem === dpr}) ? dpr : 1 
+    var scale = 1 / dpr
+    // dpr控制在已有样式中，避免错乱
+
+    metaEl.setAttribute('content', 'initial-scale=' + scale + ',maximum-scale=' + scale + ',minimum-scale=' + scale + ',user-scalable=no')
+    // 控制缩放
+
+    var remEl = document.createElement('style')
+    var rem = clientWidth / 10 * dpr
+    // rem基准为10
+
+    remEl.innerHTML = 'html{font-size:' + rem + 'px!important;'
+    docEl.firstElementChild.appendChild(remEl)
+    docEl.setAttribute('data-dpr', dpr)
+    // css hack
+  }
+  remInit()
+  // window.addEventListener('resize',function(){remInit()})
+}()
